e5fe1ef7725e68b8f04073450c463a11aee76321
유재준 <windy9300@gmail.com>
2025-12-09 10:00:14 +0900

    반복수행

--- AITest/Program.cs
+++ AITest/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] _args)
         {
             BattleMaster battleMaster = new BattleMaster();
-            battleMaster.Start();
+            var enumerator = battleMaster.Start();
+            while (enumerator.MoveNext()) { }
         }
     }
 }
